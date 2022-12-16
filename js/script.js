c5ccb21bc6cad3033b5e544e61d32490fd220c5d
KraftJunk <96074540+KraftJunk@users.noreply.github.com>
2022-12-16 21:41:37 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
-$(document).ready(function()    {
+$(window).on("load", function() {
 
-    
+    $(".loader").fadeOut(750);
+
+})
+
     $(".items").isotope({
         filter:"*",
         animationOptions:{
-        duration:1500,
-        easing: "linear",
-        queue: false
+            duration:1500,
+            easing: "linear",
+            queue: false
         }
-                                
-        });
+                            });
+
+
+$(document).ready(function()    {
+    
   
 
     $('#slides').superslides({
